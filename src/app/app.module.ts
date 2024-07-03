b7bf59f9f3967f20c794d8a82ff36296c0b7c9af
danillobr <danillorodriguesabreu@gmail.com>
2024-07-03 16:18:03 -0300

    style: CSS in layout components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { HomeComponent } from './components/pages/home/home.component';
     FooterComponent,
     AboutComponent,
     HomeComponent,
+    NewMomentComponent,
   ],
   imports: [
     BrowserModule,
