315d59e5728762519cde70a5c17b5225784b5c8a
danillobr <danillorodriguesabreu@gmail.com>
2024-07-03 16:56:36 -0300

    Revert "feat: create form page"
    This reverts commit 2db9797158109218775f37b5465ac81803e91849.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import { FooterComponent } from './components/footer/footer.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
-import { MomentFormComponent } from './components/moment-form/moment-form.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +22,6 @@ import { MomentFormComponent } from './components/moment-form/moment-form.compon
     AboutComponent,
     HomeComponent,
     NewMomentComponent,
-    MomentFormComponent,
   ],
   imports: [
     BrowserModule,
