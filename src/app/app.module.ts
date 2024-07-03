2db9797158109218775f37b5465ac81803e91849
danillobr <danillorodriguesabreu@gmail.com>
2024-07-03 16:54:24 -0300

    feat: create form page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
+import { MomentFormComponent } from './components/moment-form/moment-form.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { NewMomentComponent } from './components/pages/new-moment/new-moment.com
     AboutComponent,
     HomeComponent,
     NewMomentComponent,
+    MomentFormComponent,
   ],
   imports: [
     BrowserModule,
